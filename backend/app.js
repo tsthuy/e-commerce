de7c0b9076f849850f3d97967de894edc5a9dacd
tsthuy <huydo282003@gmail.com>
2024-09-03 16:09:56 +0700

    change endpoint of be

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use("/", (req, res) => {
+app.use("/test", (req, res) => {
   res.send("Hello World");
 });
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
