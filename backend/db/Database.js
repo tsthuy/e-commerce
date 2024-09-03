54afa5970bcbbd4d27905d528cf9cad4b996142b
tsthuy <huydo282003@gmail.com>
2024-09-03 22:19:52 +0700

    change mongdb

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -5,6 +5,7 @@ const connectDatabase = () => {
     .connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
     })
     .then((data) => {
       console.log(`mongod connected with server: ${data.connection.host}`);
