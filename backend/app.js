b98e37007db0e2949466afd74c88f62db193bd7b
tsthuy <huydo282003@gmail.com>
2024-09-28 18:01:08 +0700

    "config backend to deploy"

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
+app.use("/test", (req, res) => {
+  res.send("Hello World");
+});
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "backend/config/.env",
+    path: "config/.env",
   });
 }
 
