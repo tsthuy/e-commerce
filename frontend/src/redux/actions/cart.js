68ebb3211e03217e328e4db1fe88ad9a88214278
tsthuy <huydo282003@gmail.com>
2024-09-30 13:36:33 +0700

    clear cart after order successfully

--- frontend/src/redux/actions/cart.js
+++ frontend/src/redux/actions/cart.js
@@ -1,4 +1,4 @@
-import { addToCartRe, removeFromCartRe } from "../reducers/cart";
+import { addToCartRe, clearCart, removeFromCartRe } from "../reducers/cart";
 // add to cart
 export const addTocart = (data) => async (dispatch, getState) => {
   console.log(data);
@@ -13,4 +13,10 @@ export const removeFromCart = (data) => async (dispatch, getState) => {
   dispatch(removeFromCartRe(data._id));
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cart));
   return data;
+};
+// clear cart
+export const clearCartAction = () => async (dispatch, getState) => {
+  console.log("from hehre");
+  dispatch(clearCart());
+  localStorage.removeItem("cartItems");
 };
